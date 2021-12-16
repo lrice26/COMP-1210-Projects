0363adbe73d92012012efd77cb828dc446dc80ab
Lyndsey Rice <64554773+lrice26@users.noreply.github.com>
2021-12-16 14:32:41 -0500

    Add files via upload

---
+++ Project 1/MyLifeGoals.java
@@ -0,0 +1,32 @@
+/**
+ * Simple program to print personal life goals.
+ *
+ * Project 1
+ * @author Lyndsey Rice - COMP 1210 - D01
+ * @version August 23, 2021
+ */
+
+public class MyLifeGoals {
+   /**
+    * Prints my name and my short-term, medium-term, and long-term goals.
+    * @param args Command Line arguments - not used.
+    */
+   
+   public static void main(String[] args) {
+      
+      System.out.println("Lyndsey Rice");
+      System.out.println();
+      System.out.println("In the short-term, I want to be successful in 3 ways:"
+         + " make close friends and experiences, academic success in classes,"
+         + " and have good physical health."); 
+      System.out.println("In the next 5 years, these are my goals: "
+         + "graduate college, get an enjoyable and well-paying job,"
+         + " and have two dogs, named Chad and Troy."); 
+      System.out.println("By the end of my life, here are my goals: "
+         + "get married and raise a family, travel to lots of places, "
+         + "and feel satisfied with how I've lived.");          
+         
+   
+   }
+
+}
